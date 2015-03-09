60e437dc9b548a0ebdf4dd93d9fa159dbc23c45e
freedee12 <TeddyDubno@Teddys-MacBook-Pro.local>
2015-03-09 17:41:48 -0400

    ReInitialcommit

---
+++ SwipeBall/HighScore.swift
@@ -0,0 +1,16 @@
+//
+//  HighScore.swift
+//  SwipeBall
+//
+//  Created by Teddy Dubno on 2/18/15.
+//  Copyright (c) 2015 Teddy Dubno. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class HighScore: NSManagedObject {
+
+    @NSManaged var hS: NSNumber
+
+}
